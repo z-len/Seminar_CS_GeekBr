d9403f7aea1bf930b0de0cecdc9fa0d953a4eb1f
Elena Zosina <z-len@yandex.ru>
2023-01-29 14:28:20 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int GetSum(int num)
+{
+    int sum = 0;
+    int tail = 0;
+    while(num>0)
+    {
+        tail = num%10;
+        sum +=tail;
+        num = num/10;
+       }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int summ = GetSum(number);
+Console.WriteLine($" Cумма цифр {number} равна {summ}.");
